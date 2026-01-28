1b7477de93c0734dc4a2e86e7676c96c582ce28e
Tianqi Qiu <tianqiqi@usc.edu>
2026-01-28 08:30:59 +0800

    Add files via upload

---
+++ Lab3/scripts/quick_test.bat
@@ -0,0 +1,9 @@
+@echo off
+echo === CPU Matrix Multiplication Tests ===
+echo.
+matrix_cpu.exe 256
+matrix_cpu.exe 512
+matrix_cpu.exe 768
+matrix_cpu.exe 1024
+matrix_cpu.exe 1536
+matrix_cpu.exe 2048
