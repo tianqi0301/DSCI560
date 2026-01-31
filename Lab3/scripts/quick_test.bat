0392b7f8127074845c19729bd6ab47069182b343
Tianqi Qiu <tianqiqi@usc.edu>
2026-01-30 18:24:49 -0800

    Delete Lab3/scripts/quick_test.bat

--- Lab3/scripts/quick_test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo === CPU Matrix Multiplication Tests ===
-echo.
-matrix_cpu.exe 256
-matrix_cpu.exe 512
-matrix_cpu.exe 768
-matrix_cpu.exe 1024
-matrix_cpu.exe 1536
-matrix_cpu.exe 2048
